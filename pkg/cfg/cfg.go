f62bd5f2f4b2b2f40af77955790b0f78adb05727
Aaron Parecki <aaron@parecki.com>
2017-10-08 18:27:13 -0700

    allow forcing the domain of the cookie to set

--- pkg/cfg/cfg.go
+++ pkg/cfg/cfg.go
@@ -23,6 +23,7 @@ type CfgT struct {
 	}
 	Cookie struct {
 		Name     string `mapstructure:"name"`
+		Domain   string `mapstructure:"domain"`
 		Secure   bool   `mapstructure:"secure"`
 		HTTPOnly bool   `mapstructure:"httpOnly"`
 	}
