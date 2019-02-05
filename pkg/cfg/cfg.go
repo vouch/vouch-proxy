62c9cfed654d55cc3fd93dee9d93c1d2a530dfe4
Simon Gottschlag <simon.gottschlag@xenit.se>
2019-02-05 12:48:54 +0100

    debug unmarshal key

--- pkg/cfg/cfg.go
+++ pkg/cfg/cfg.go
@@ -184,6 +184,7 @@ func ParseConfig() {
 		panic(err)
 	}
 	UnmarshalKey(Branding.LCName, &Cfg)
+	log.Debugf("debug unmarshal Cfg: %v", Cfg)
 	if len(Cfg.Domains) == 0 {
 		// then lets check for "lasso"
 		var oldConfig config
