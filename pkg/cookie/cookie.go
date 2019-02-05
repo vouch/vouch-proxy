aa65c7b1eac173336dbdd4d32bbce8bf9e560fdb
Simon Gottschlag <simon@gottschlag.se>
2019-02-05 10:42:49 +0100

    test Cfg

--- pkg/cookie/cookie.go
+++ pkg/cookie/cookie.go
@@ -24,8 +24,8 @@ func setCookie(w http.ResponseWriter, r *http.Request, val string, maxAge int) {
 	}
 	domain := domains.Matches(r.Host)
 	// Allow overriding the cookie domain in the config file
-	log.Debugf("temp debug - cookie domain: %v", cfg.Cfg.Cookie.Domain)
-	log.Debugf("temp debug - cfg: %v", cfg.Cfg)
+	log.Debugf("temp debug - cookie domain: %v", Cfg.Cookie.Domain)
+	log.Debugf("temp debug - cfg: %v", Cfg)
 	if cfg.Cfg.Cookie.Domain != "" {
 		domain = cfg.Cfg.Cookie.Domain
 		log.Debugf("setting the cookie domain to %v", domain)
