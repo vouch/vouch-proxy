b7d070da50cea2c4278e4d4e7e4aaa61de4b5aa2
Simon Gottschlag <simon.gottschlag@xenit.se>
2019-02-05 12:20:41 +0100

    change cfg.Cfg to config

--- pkg/cookie/cookie.go
+++ pkg/cookie/cookie.go
@@ -11,8 +11,8 @@ import (
 	"github.com/spf13/viper"
 )
 
-var defaultMaxAge = cfg.Cfg.JWT.MaxAge * 60
-var cookieConfig = viper.AllSettings()
+var defaultMaxAge = config.JWT.MaxAge * 60
+var config = viper.AllSettings()
 
 // SetCookie http
 func SetCookie(w http.ResponseWriter, r *http.Request, val string) {
@@ -26,30 +26,30 @@ func setCookie(w http.ResponseWriter, r *http.Request, val string, maxAge int) {
 	}
 	domain := domains.Matches(r.Host)
 	// Allow overriding the cookie domain in the config file
-	log.Debugf("temp debug - cookie domain: %v", cfg.Cfg.Cookie.Domain)
-	log.Debugf("temp debug - cookie: %v", cfg.Cfg.Cookie)
+	log.Debugf("temp debug - cookie domain: %v", config.Cookie.Domain)
+	log.Debugf("temp debug - cookie: %v", config.Cookie)
 	log.Debugf("temp debug - cfg: %v", cfg.Cfg)
 	log.Debugf("temp debug - host: %v", r.Host)
 	log.Debugf("temp debug - cookieConfig: %v", cookieConfig)
-	if cfg.Cfg.Cookie.Domain != "" {
-		domain = cfg.Cfg.Cookie.Domain
+	if config.Cookie.Domain != "" {
+		domain = config.Cookie.Domain
 		log.Debugf("setting the cookie domain to %v", domain)
 	}
-	// log.Debugf("cookie %s expires %d", cfg.Cfg.Cookie.Name, expires)
+	// log.Debugf("cookie %s expires %d", config.Cookie.Name, expires)
 	http.SetCookie(w, &http.Cookie{
-		Name:     cfg.Cfg.Cookie.Name,
+		Name:     config.Cookie.Name,
 		Value:    val,
 		Path:     "/",
 		Domain:   domain,
 		MaxAge:   maxAge,
-		Secure:   cfg.Cfg.Cookie.Secure,
-		HttpOnly: cfg.Cfg.Cookie.HTTPOnly,
+		Secure:   config.Cookie.Secure,
+		HttpOnly: config.Cookie.HTTPOnly,
 	})
 }
 
 // Cookie get the vouch jwt cookie
 func Cookie(r *http.Request) (string, error) {
-	cookie, err := r.Cookie(cfg.Cfg.Cookie.Name)
+	cookie, err := r.Cookie(config.Cookie.Name)
 	if err != nil {
 		return "", err
 	}
@@ -58,7 +58,7 @@ func Cookie(r *http.Request) (string, error) {
 	}
 
 	log.WithFields(log.Fields{
-		"cookieName":  cfg.Cfg.Cookie.Name,
+		"cookieName":  config.Cookie.Name,
 		"cookieValue": cookie.Value,
 	}).Debug("cookie")
 	return cookie.Value, err
