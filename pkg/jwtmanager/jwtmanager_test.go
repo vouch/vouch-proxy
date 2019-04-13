0adc4503f84b00e9772cbccd83516af679be0996
artagel <dan@syssec.biz>
2019-04-13 20:43:23 +0400

    Update jwt tests to support claims.

--- pkg/jwtmanager/jwtmanager_test.go
+++ pkg/jwtmanager/jwtmanager_test.go
@@ -1,10 +1,10 @@
 package jwtmanager
 
 import (
-	"testing"
-
+	"encoding/json"
 	"github.com/vouch/vouch-proxy/pkg/cfg"
 	"github.com/vouch/vouch-proxy/pkg/structs"
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -16,6 +16,15 @@ var (
 	}
 
 	lc VouchClaims
+
+	claimjson = `{
+		"sub": "f:a95afe53-60ba-4ac6-af15-fab870e72f3d:mrtester",
+		"groups": ["Website Users", "Test Group"],
+		"given_name": "Mister",
+		"family_name": "Tester",
+		"email": "mrtester@test.int"
+	}`
+	customClaims = structs.CustomClaims{}
 )
 
 func init() {
@@ -26,13 +35,15 @@ func init() {
 	lc = VouchClaims{
 		u1.Username,
 		Sites,
+		customClaims.Claims,
 		StandardClaims,
 	}
+	json.Unmarshal([]byte(claimjson), &customClaims.Claims)
 }
 
 func TestCreateUserTokenStringAndParseToUsername(t *testing.T) {
 
-	uts := CreateUserTokenString(u1)
+	uts := CreateUserTokenString(u1, customClaims)
 	assert.NotEmpty(t, uts)
 
 	utsParsed, err := ParseTokenString(uts)
@@ -57,7 +68,7 @@ func TestClaims(t *testing.T) {
 	// log.Infof("lc d %s", d.String())
 	// lc.StandardClaims.ExpiresAt = now.Add(time.Duration(ExpiresAtMinutes) * time.Minute).Unix()
 	// log.Infof("lc expiresAt %d", now.Unix()-lc.StandardClaims.ExpiresAt)
-	uts := CreateUserTokenString(u1)
+	uts := CreateUserTokenString(u1, customClaims)
 	utsParsed, _ := ParseTokenString(uts)
 	log.Infof("utsParsed: %+v", utsParsed)
 	log.Infof("Sites: %+v", Sites)
