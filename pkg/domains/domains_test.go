cfc36082bf0e2180a129e389b4f55c371ce60633
Eike Hartmann <info@eikehartmann.de>
2020-01-26 12:03:21 +0100

    Fix domain checks

---
+++ pkg/domains/domains_test.go
@@ -0,0 +1,42 @@
+package domains
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+
+	"github.com/vouch/vouch-proxy/pkg/cfg"
+)
+
+func init() {
+	cfg.InitForTestPurposes()
+	cfg.Cfg.Domains = []string{"vouch.github.io", "sub.test.mydomain.com", "test.mydomain.com"}
+	Refresh()
+}
+
+func TestIsUnderManagement(t *testing.T) {
+	assert.True(t, IsUnderManagement("test@vouch.github.io"))
+	assert.True(t, IsUnderManagement("test@sub.vouch.github.io"))
+	assert.True(t, IsUnderManagement("test@test.mydomain.com"))
+	assert.True(t, IsUnderManagement("test@sub.test.mydomain.com"))
+
+	assert.False(t, IsUnderManagement("test@example.com"))
+	assert.False(t, IsUnderManagement("vouch.github.io@example.com"))
+	assert.False(t, IsUnderManagement("test-vouch.github.io@example.com"))
+	assert.False(t, IsUnderManagement("test@vouch.github.io.com"))
+}
+
+func TestMatches(t *testing.T) {
+	// Full email should not be accepted
+	assert.Equal(t, "", Matches("test@vouch.github.io"))
+	
+	assert.Equal(t, "vouch.github.io", Matches("vouch.github.io"))
+	assert.Equal(t, "vouch.github.io", Matches("sub.vouch.github.io"))
+	assert.Equal(t, "", Matches("a-different-vouch.github.io"))
+
+	assert.Equal(t, "", Matches("mydomain.com"))
+	
+	assert.Equal(t, "test.mydomain.com", Matches("test.mydomain.com"))
+	assert.Equal(t, "sub.test.mydomain.com", Matches("sub.test.mydomain.com"))
+	assert.Equal(t, "sub.test.mydomain.com", Matches("subsub.sub.test.mydomain.com"))
+	assert.Equal(t, "test.mydomain.com", Matches("other.test.mydomain.com"))
+}
