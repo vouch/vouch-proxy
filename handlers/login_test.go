c30dad97da05d091f9626d800ec5cbb239da2bac
Benjamin Foote <git@bnf.net>
2020-04-30 19:09:00 -0700

    test redirected URL param

--- handlers/login_test.go
+++ handlers/login_test.go
@@ -1,27 +1,40 @@
 package handlers
 
-import "testing"
+import (
+	"net/http"
+	"net/url"
+	"testing"
+)
 
-func Test_validateRequestedURL(t *testing.T) {
+func Test_getValidRequestedURL(t *testing.T) {
+	setUp("/config/testing/handler_login_url.yml")
+	r := &http.Request{}
 	tests := []struct {
 		name    string
 		url     string
+		want    string
 		wantErr bool
 	}{
-		{"no https", "example.com/dest", true},
-		{"redirection chaining", "http://example.com/dest?url=https://", true},
-		{"data uri", "http://example.com/dest?url=data:text/plain,Example+Text", true},
+		{"no https", "example.com/dest", "", true},
+		{"redirection chaining", "http://example.com/dest?url=https://", "", true},
+		{"data uri", "http://example.com/dest?url=data:text/plain,Example+Text", "", true},
+		{"not in domain", "http://somewherelse.com/", "", true},
+		{"should warn", "https://example.com/", "https://example.com/", false},
+		{"should be fine", "http://example.com/", "http://example.com/", false},
+
+		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			err := validateRequestedURL(tt.url)
+			r.URL, _ = url.Parse("http://vouch.example.com/login?url=" + tt.url)
+			got, err := getValidRequestedURL(r)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("cleanRequestedURL() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("getValidRequestedURL() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			// if got != tt.want {
-			// 	t.Errorf("cleanRequestedURL() = %v, want %v", got, tt.want)
-			// }
+			if got != tt.want {
+				t.Errorf("getValidRequestedURL() = %v, want %v", got, tt.want)
+			}
 		})
 	}
 }
