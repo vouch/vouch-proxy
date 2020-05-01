96fe9ce368b4e6aac501523ad1907b76089cbdcc
Benjamin Foote <git@bnf.net>
2020-04-30 19:07:15 -0700

    general cleanup, err400 method

--- handlers/responses.go
+++ handlers/responses.go
@@ -1,37 +1,23 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/vouch/vouch-proxy/pkg/cfg"
 	"github.com/vouch/vouch-proxy/pkg/cookie"
 )
 
-// authError sets the values to return to nginx
-type authError struct {
-	Error string
-	JWT   string
-}
-
 func renderIndex(w http.ResponseWriter, msg string) {
 	if err := indexTemplate.Execute(w, &Index{Msg: msg, TestURLs: cfg.Cfg.TestURLs, Testing: cfg.Cfg.Testing}); err != nil {
 		log.Error(err)
 	}
 }
-
-// the standard error
-// this is captured by nginx, which converts the 401 into 302 to the login page
-func error401(w http.ResponseWriter, r *http.Request, ae authError) {
-	log.Error(ae.Error)
-	cookie.ClearCookie(w, r)
-	// w.Header().Set("X-Vouch-Error", ae.Error)
-	http.Error(w, ae.Error, http.StatusUnauthorized)
-	// TODO put this back in place if multiple auth mechanism are available
-	// c.HTML(http.StatusBadRequest, "error.tmpl", gin.H{"message": errStr})
-}
-
-func error401na(w http.ResponseWriter, r *http.Request) {
-	error401(w, r, authError{Error: "not authorized"})
+func ok200(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("200 OK\n"))
+	if err != nil {
+		log.Error(err)
+	}
 }
 
 func redirect302(w http.ResponseWriter, r *http.Request, rURL string) {
@@ -43,9 +29,24 @@ func redirect302(w http.ResponseWriter, r *http.Request, rURL string) {
 	http.Redirect(w, r, rURL, http.StatusFound)
 }
 
-func ok200(w http.ResponseWriter, r *http.Request) {
-	_, err := w.Write([]byte("200 OK\n"))
-	if err != nil {
-		log.Error(err)
-	}
+// 400 Bad Request
+// returned when the requesed url param for /login or /logout is bd
+func error400(w http.ResponseWriter, r *http.Request, e error) {
+	log.Error(e)
+	cookie.ClearCookie(w, r)
+	w.Header().Set("X-Vouch-Error", e.Error())
+	http.Error(w, e.Error(), http.StatusUnauthorized)
+}
+
+// the standard error
+// this is captured by nginx, which converts the 401 into 302 to the login page
+func error401(w http.ResponseWriter, r *http.Request, e error) {
+	log.Error(e)
+	cookie.ClearCookie(w, r)
+	w.Header().Set("X-Vouch-Error", e.Error())
+	http.Error(w, e.Error(), http.StatusUnauthorized)
+}
+
+func error401na(w http.ResponseWriter, r *http.Request) {
+	error401(w, r, fmt.Errorf("not authorized"))
 }
