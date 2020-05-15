da1f464c619c92f01af8a768a6abecb79929bc2d
Benjamin Foote <git@bnf.net>
2020-05-15 01:02:12 -0700

    mv providers from handlers to pkg/providers

--- pkg/providers/google/google.go
+++ pkg/providers/google/google.go
@@ -1,22 +1,22 @@
 /*
 
 Copyright 2020 The Vouch Proxy Authors.
-Use of this source code is governed by The MIT License (MIT) that 
-can be found in the LICENSE file. Software distributed under The 
+Use of this source code is governed by The MIT License (MIT) that
+can be found in the LICENSE file. Software distributed under The
 MIT License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
 OR CONDITIONS OF ANY KIND, either express or implied.
 
 */
 
-package openid
+package google
 
 import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
 
-	"github.com/vouch/vouch-proxy/handlers/common"
 	"github.com/vouch/vouch-proxy/pkg/cfg"
+	"github.com/vouch/vouch-proxy/pkg/providers/common"
 	"github.com/vouch/vouch-proxy/pkg/structs"
 	"go.uber.org/zap"
 )
@@ -47,7 +47,7 @@ func (Provider) GetUserInfo(r *http.Request, user *structs.User, customClaims *s
 		}
 	}()
 	data, _ := ioutil.ReadAll(userinfo.Body)
-	log.Infof("OpenID userinfo body: %s", string(data))
+	log.Infof("google userinfo body: ", string(data))
 	if err = common.MapClaims(data, customClaims); err != nil {
 		log.Error(err)
 		return err
@@ -57,5 +57,6 @@ func (Provider) GetUserInfo(r *http.Request, user *structs.User, customClaims *s
 		return err
 	}
 	user.PrepareUserData()
+
 	return nil
 }
