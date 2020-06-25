054bff3fdfc8870c208eb5a5711b7e937cfc200c
Benjamin Foote <git@bnf.net>
2020-06-25 13:25:58 -0700

    Squashed commit of the following:
    commit aed84160d4b9737d09b127a2c022afda83e1cfd9
    Author: Benjamin Foote <git@bnf.net>
    Date:   Thu Jun 25 13:23:31 2020 -0700
    
        #281 gofmt, minor edits
    
    commit c38b05da608becb4cc59666ce650e38002799cbd
    Author: Benjamin Foote <git@bnf.net>
    Date:   Thu Jun 25 13:23:15 2020 -0700
    
        #281 compile regex once, use cfg.Branding
    
    commit d9cb3f6005e043a31292930c66d336e87447f11c
    Author: Benjamin Foote <git@bnf.net>
    Date:   Thu Jun 25 13:20:01 2020 -0700
    
        error on empty URL
    
    commit 4adbaadf2870dc9381a8e62cc79ca218aff0a2e7
    Author: Ben Artin <ben@artins.org>
    Date:   Thu Jun 25 12:57:06 2020 -0400
    
        Fixed handling of multiple vouch params in login request
    
    commit dfc6cb33a74f64b44b2c4d0d7caed4c22f96df5e
    Author: Ben Artin <ben@artins.org>
    Date:   Thu Jun 25 12:59:24 2020 -0400
    
        Allow ‘error’ param
    
    commit e50e1aa44058dd75f7cdf8cd9d69f61ab45b6dca
    Author: Ben Artin <ben@artins.org>
    Date:   Thu Jun 25 12:29:16 2020 -0400
    
        Clarified README unit tests
    
    commit dabee5c27efb106de99a60c9e9651eab11287583
    Author: Ben Artin <ben@artins.org>
    Date:   Thu Jun 25 12:28:32 2020 -0400
    
        Log about url normalization
    
    commit c1b37a7da943b61a0646558c606425ed5ee8d995
    Author: Benjamin Foote <git@bnf.net>
    Date:   Wed Jun 24 12:17:02 2020 -0700
    
        #281 add tests for README 302 syntax
    
    commit ddbea36d37c78341d29e0dd8e17b160b9b28e698
    Author: Ben Artin <ben@artins.org>
    Date:   Tue Jun 23 17:23:25 2020 -0400
    
        Allow semicolon as param separator in URL
    
        https://www.w3.org/TR/html401/appendix/notes.html#h-B.2.2
    
    commit f27d4b1df5cad32518ad5ecccce9284147512185
    Author: Ben Artin <ben@artins.org>
    Date:   Tue Jun 23 16:59:31 2020 -0400
    
        Added tests for the case of vouch param before url param
    
    commit 5b4c71d9b0af239936f76501968a9611ee86b317
    Author: Ben Artin <ben@artins.org>
    Date:   Tue Jun 23 16:59:05 2020 -0400
    
        Unambiguous test name
    
    commit 881dfc2c70edfe7c4fda11907571420161f0ccc3
    Author: Ben Artin <ben@artins.org>
    Date:   Sat Jun 20 04:21:01 2020 -0400
    
        More robust parsing of url param to login; see #281

--- handlers/login_test.go
+++ handlers/login_test.go
@@ -20,6 +20,67 @@ import (
 	"github.com/vouch/vouch-proxy/pkg/cfg"
 )
 
+func Test_normalizeLoginURL(t *testing.T) {
+	setUp("/config/testing/handler_login_url.yml")
+	tests := []struct {
+		name    string
+		url     string
+		want    string
+		wantErr bool
+	}{
+		// This is not an RFC-compliant URL because it does not encode :// in the url param; we accept it anyway
+		{"extra params", "http://host/login?url=http://host/path?p2=2", "http://host/path?p2=2", false},
+		{"extra params (blank)", "http://host/login?url=http://host/path?p2=", "http://host/path?p2=", false},
+		// This is not an RFC-compliant URL because it does not encode :// in the url param; we accept it anyway
+		// Even though the p1 param is not a login param, we do not interpret is as part of the url param because it precedes it
+		{"prior params", "http://host/login?p1=1&url=http://host/path", "http://host/path", true},
+		// This is not an RFC-compliant URL because it does not encode :// in the url param; we accept it anyway
+		// We assume vouch-* is a login param and do not fold it into url
+		{"vouch-* params after", "http://host/login?url=http://host/path&vouch-xxx=2", "http://host/path", false},
+		// This is not an RFC-compliant URL because it does not encode :// in the url param; we accept it anyway
+		// We assume vouch-* is a login param and do not fold it into url
+		{"vouch-* params before", "http://host/login?vouch-xxx=1&url=http://host/path", "http://host/path", false},
+		// This is not an RFC-compliant URL because it does not encode :// in the url param; we accept it anyway
+		// We assume x-vouch-* is a login param and do not fold it into url
+		{"x-vouch-* params after", "http://host/login?url=http://host/path&vouch-xxx=2", "http://host/path", false},
+		// This is not an RFC-compliant URL because it does not encode :// in the url param; we accept it anyway
+		// We assume x-vouch-* is a login param and do not fold it into url
+		{"x-vouch-* params before", "http://host/login?x-vouch-xxx=1&url=http://host/path", "http://host/path", false},
+		// This is not an RFC-compliant URL because it does not encode :// in the url param; we accept it anyway
+		// Even though p1 is not a login param, we do not interpret is as part of url because it follows a login param (vouch-*)
+		{"params after vouch-* params", "http://host/login?url=http://host/path&vouch-xxx=2&p3=3", "http://host/path", true},
+		// This is not an RFC-compliant URL because it does not encode :// in the url param; we accept it anyway
+		// Even though p1 is not a login param, we do not interpret is as part of url because it follows a login param (x-vouch-*)
+		{"params after x-vouch-* params", "http://host/login?url=http://host/path&x-vouch-xxx=2&p3=3", "http://host/path", true},
+		// This is not an RFC-compliant URL; it combines all the aspects above
+		{"all params", "http://host/login?p1=1&url=http://host/path?p2=2&p3=3&x-vouch-xxx=4&vouch=5&error=6&p7=7", "http://host/path?p2=2&p3=3", true},
+		// This is an RFC-compliant URL
+		{"all params (encoded)", "http://host/login?p1=1&url=http%3a%2f%2fhost/path%3fp2=2%26p3=3&x-vouch-xxx=4&vouch=5&error=6&p7=7", "http://host/path?p2=2&p3=3", true},
+		// This is not an RFC-compliant URL; it combines all the aspects above, and it uses semicolons as parameter separators
+		// Note that when we fold a stray param into the url param, we always do so with &s
+		{"all params (semicolons)", "http://host/login?p1=1;url=http://host/path?p2=2;p3=3;x-vouch-xxx=4;p5=5", "http://host/path?p2=2&p3=3", true},
+		// This is an RFC-compliant URL that uses semicolons as parameter separators
+		{"all params (encoded, semicolons)", "http://host/login?p1=1;url=http%3a%2f%2fhost/path%3fp2=2%3bp3=3;x-vouch-xxx=4;p5=5", "http://host/path?p2=2;p3=3", true},
+		// Real world tests
+		// since v0.4.0 the vouch README has specified an Nginx config including a 302 redirect in the following format...
+		{"Vouch README (with error)", "http://host/login?url=http://host/path?p2=2&vouch-failcount=3&X-Vouch-Token=TOKEN&error=anerror", "http://host/path?p2=2", false},
+		{"Vouch README (blank error)", "http://host/login?url=http://host/path?p2=2&vouch-failcount=&X-Vouch-Token=&error=", "http://host/path?p2=2", false},
+		{"Vouch README (semicolons, blank error)", "http://host/login?url=http://host/path?p2=2;p3=3&vouch-failcount=&X-Vouch-Token=&error=", "http://host/path?p2=2&p3=3", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			u, _ := url.Parse(tt.url)
+			got, err := normalizeLoginURLParam(u)
+			if got.String() != tt.want {
+				t.Errorf("normalizeLoginURLParam() = %v, want %v", got, tt.want)
+			}
+			if (err != nil) != tt.wantErr {
+				t.Errorf("normalizeLoginURLParam() err = %v", err)
+			}
+		})
+	}
+}
+
 func Test_getValidRequestedURL(t *testing.T) {
 	setUp("/config/testing/handler_login_url.yml")
 	r := &http.Request{}
@@ -33,6 +94,7 @@ func Test_getValidRequestedURL(t *testing.T) {
 		{"redirection chaining", "http://example.com/dest?url=https://", "", true},
 		{"redirection chaining upper case", "http://example.com/dest?url=HTTPS://someplaceelse.com", "", true},
 		{"redirection chaining no protocol", "http://example.com/dest?url=//someplaceelse.com", "", true},
+		{"redirection chaining escaped https://", "http://example.com/dest?url=https%3a%2f%2fsomeplaceelse.com", "", true},
 		{"data uri", "http://example.com/dest?url=data:text/plain,Example+Text", "", true},
 		{"javascript uri", "http://example.com/dest?url=javascript:alert(1)", "", true},
 		{"not in domain", "http://somewherelse.com/", "", true},
@@ -40,8 +102,7 @@ func Test_getValidRequestedURL(t *testing.T) {
 		{"should be fine", "http://example.com/", "http://example.com/", false},
 		{"multiple query param", "http://example.com/?strange=but-true&also-strange=but-false", "http://example.com/?strange=but-true&also-strange=but-false", false},
 		{"multiple query params, one of them bad", "http://example.com/?strange=but-true&also-strange=but-false&strange-but-bad=https://badandstrange.com", "", true},
-
-		// TODO: Add test cases.
+		{"multiple query params, one of them bad (escaped)", "http://example.com/?strange=but-true&also-strange=but-false&strange-but-bad=https%3a%2f%2fbadandstrange.com", "", true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
