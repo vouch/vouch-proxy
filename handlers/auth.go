aea63c95c92a4255d19e2eb3bc7d4aca386397ff
Darius Asri Rezaei <darius.asri.rezaei@ibm.com>
2020-10-14 13:38:30 -0400

    fix: logic

--- handlers/auth.go
+++ handlers/auth.go
@@ -36,7 +36,7 @@ var (
 var emailRegex = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 // isEmailValid checks if the email provided passes the required structure and length.
 func isEmailValid(e string) bool {
-	if len(e) < 3 && len(e) > 254 {
+	if len(e) < 3 || len(e) > 254 {
 		return false
 	}
 	return emailRegex.MatchString(e)
