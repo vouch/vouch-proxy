e5fa4ca812baf0ad846ca509cd160ee549b3db11
Oliver Wittkopf <gitlab@4ward.wittkopf.eu>
2020-12-22 02:39:06 +0100

    sort by length not name

--- pkg/domains/domains.go
+++ pkg/domains/domains.go
@@ -23,7 +23,7 @@ var log *zap.SugaredLogger
 // Configure see main.go configure()
 func Configure() {
 	log = cfg.Logging.Logger
-	sort.Sort(ByUri(cfg.Cfg.Domains))
+	sort.Sort(ByUriLengthDesc(cfg.Cfg.Domains))
 }
 
 // IsUnderManagement check if an email is under vouch-managed domain
@@ -43,16 +43,16 @@ func IsUnderManagement(email string) bool {
 
 // ByLengthDesc sort from
 // https://play.golang.org/p/N6GbEgBffd
-type ByUri cfg.DomainsOptions
+type ByUriLengthDesc cfg.DomainsOptions
 
-func (s ByUri) Len() int {
+func (s ByUriLengthDesc) Len() int {
 	return len(s)
 }
-func (s ByUri) Swap(i, j int) {
+func (s ByUriLengthDesc) Swap(i, j int) {
 	s[i], s[j] = s[j], s[i]
 }
 
-// this differs by sorting by Uri
-func (s ByUri) Less(i, j int) bool {
-	return s[j].Uri < s[i].Uri
+// this differs by offing the longest first
+func (s ByUriLengthDesc) Less(i, j int) bool {
+	return len(s[j].Uri) < len(s[i].Uri)
 }
