db40d4f913d08b198e2f1924d7de05fbf648e740
Benjamin Foote <git@bnf.net>
2021-05-20 13:06:14 -0700

    make sure JWT expires from cache by jwt.expiration

--- pkg/jwtmanager/jwtcache.go
+++ pkg/jwtmanager/jwtcache.go
@@ -23,15 +23,15 @@ import (
 
 // Cache in memory temporary store for responses from /validate for jwt
 var Cache *cache.Cache
+var expire int = 20 // default 20 minutes
+var dExp time.Duration
 
 func cacheConfigure() {
-
-	var expire int = 20 // default 20 minutes
 
 	if cfg.Cfg.JWT.MaxAge < expire {
 		expire = cfg.Cfg.JWT.MaxAge
 	}
-	dExp := time.Duration(expire) * time.Minute
+	dExp = time.Duration(expire) * time.Minute
 	purgeCheck := dExp / 5
 	// log.Debugf("cacheConfigure expire %d dExp %d purgecheck %d", expire, dExp, purgeCheck)
 	Cache = cache.New(dExp, purgeCheck)
@@ -46,16 +46,13 @@ func cacheConfigure() {
 
 // JWTCacheHandler looks for a JWT and...
 // returns a cached response
-// or passes the JWT in the context
-// tests for JWTCacheHandler are present in `handlers/validate_test.go` to avoid circular imports
+// or passes the request to /validate
+// all tests for JWTCacheHandler are present in `handlers/validate_test.go` to avoid circular imports
 func JWTCacheHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// return http.HandlerFunc(func(w CaptureWriter, r *http.Request) {
-
-		// wrap ResponseWriter
-		// v := CachedResponse{CaptureWriter: &CaptureWriter{ResponseWriter: w}}
 
 		jwt := FindJWT(r)
+
 		// check to see if we have headers cached for this jwt
 		if jwt != "" {
 			if resp, found := Cache.Get(jwt); found {
@@ -82,7 +79,28 @@ func JWTCacheHandler(next http.Handler) http.Handler {
 			// r.Context().Done() is still open
 			// cache the response headers for this jwt
 			// log.Debug("setting cache for %+v", w.Header().Clone())
-			Cache.SetDefault(jwt, w.Header().Clone())
+
+			claims, err := ClaimsFromJWT(jwt)
+			now := time.Now().Unix()
+			if err != nil {
+				log.Error("very unusual error, we found a jwt for /validate but we couldn't parse it for claims while setting it into cache, returning")
+				return
+				// log.Debugf("*HERE* claims expire, time.now.unix, dExp %d - %d = %d > %d", claims.ExpiresAt, now, claims.ExpiresAt-now, int64(dExp))
+				// log.Debugf("*HERE* time.Duration((claims.ExpiresAt-time.Now().Unix())*time.Second.Nanoseconds()) %d", time.Duration((claims.ExpiresAt-time.Now().Unix())*time.Second.Nanoseconds()))
+			}
+
+			// first see if the jwt's expiration will arrive before the cache expiration
+			// if this jwt expires in 10 minutes then we don't want to cache it for 20
+			// this might happen if the jwt expiration is set to 240 minutes, and the user last logged into the IdP 230 minutes ago
+			// then the user went away, cache was purged and now they return with 10 minutes left before token expiration
+			if !claims.VerifyExpiresAt(now+int64(dExp/time.Second), true) {
+				jwtExpiresIn := time.Duration((claims.ExpiresAt - now) * int64(time.Second))
+				log.Debugf("cache default expiration (%d) is after claim expiration (%d). setting cache experation to claim expiration for this entry", dExp, jwtExpiresIn)
+				Cache.Set(jwt, w.Header().Clone(), jwtExpiresIn)
+			} else {
+				Cache.SetDefault(jwt, w.Header().Clone())
+			}
+
 		}
 	})
 }
