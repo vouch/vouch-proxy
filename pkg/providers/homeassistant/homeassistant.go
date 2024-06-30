afd511df7deea845306238a2befaa6ff5584b941
Ivan Kara <ivanjuan222@gmail.com>
2024-06-30 08:28:53 +0700

    add home assistant username fetch via websocket

--- pkg/providers/homeassistant/homeassistant.go
+++ pkg/providers/homeassistant/homeassistant.go
@@ -12,10 +12,11 @@ package homeassistant
 
 import (
 	"encoding/json"
-	"golang.org/x/oauth2"
-	"io/ioutil"
 	"net/http"
 
+	"golang.org/x/oauth2"
+
+	"github.com/gorilla/websocket"
 	"github.com/vouch/vouch-proxy/pkg/cfg"
 	"github.com/vouch/vouch-proxy/pkg/providers/common"
 	"github.com/vouch/vouch-proxy/pkg/structs"
@@ -32,30 +33,67 @@ func (Provider) Configure() {
 	log = cfg.Logging.Logger
 }
 
+type AuthMessage struct {
+	Type  string `json:"type"`
+	Token string `json:"auth_token"`
+}
+
+type RequestMessage struct {
+	Id   int    `json:"id"`
+	Type string `json:"type"`
+}
+
+type ResponseMessage struct {
+	Id     int                       `json:"id"`
+	Result structs.HomeAssistantUser `json:"result"`
+}
+
 // GetUserInfo provider specific call to get userinfomation
-// More info: https://developers.home-assistant.io/docs/en/auth_api.html
+// More info: https://github.com/home-assistant/core/blob/5280291f98db41b6edd822a6b2fe6df4dea3df6a/homeassistant/components/auth/__init__.py#L484
+// Websocket API info: https://developers.home-assistant.io/docs/api/websocket
 func (Provider) GetUserInfo(r *http.Request, user *structs.User, customClaims *structs.CustomClaims, ptokens *structs.PTokens, opts ...oauth2.AuthCodeOption) (rerr error) {
-	client, providerToken, err := common.PrepareTokensAndClient(r, ptokens, false, opts...)
+	_, providerToken, err := common.PrepareTokensAndClient(r, ptokens, false, opts...)
 	if err != nil {
 		return err
 	}
 	ptokens.PAccessToken = providerToken.Extra("access_token").(string)
-	userinfo, err := client.Get(cfg.GenOAuth.UserInfoURL)
+
+	wsURL := cfg.GenOAuth.UserInfoURL
+	client, _, err := websocket.DefaultDialer.Dial(wsURL, nil)
 	if err != nil {
 		return err
 	}
-	defer func() {
-		if err := userinfo.Body.Close(); err != nil {
-			rerr = err
-		}
-	}()
-	data, _ := ioutil.ReadAll(userinfo.Body)
-	log.Infof("HA userinfo body: %s", string(data))
-	haUser := structs.User{}
-	if err = json.Unmarshal(data, &haUser); err != nil {
-		log.Error(err)
+	defer client.Close()
+
+	authMessage := AuthMessage{
+		Type:  "auth",
+		Token: ptokens.PAccessToken,
+	}
+	if err := client.WriteJSON(authMessage); err != nil {
 		return err
 	}
-	user.Username = haUser.Username
+	_, _, err = client.ReadMessage()
+	if err != nil {
+		return err
+	}
+
+	requestMessage := RequestMessage{
+		Id:   10, // Can be any number but must be increased on each request
+		Type: "auth/current_user",
+	}
+	if err := client.WriteJSON(requestMessage); err != nil {
+		return err
+	}
+	_, responseMessage, err := client.ReadMessage()
+	if err != nil {
+		return err
+	}
+	log.Infof("HA userinfo body: %s", string(responseMessage))
+	var data ResponseMessage
+	if err := json.Unmarshal(responseMessage, &data); err != nil {
+		return err
+	}
+	data.Result.PrepareUserData()
+	user.Username = data.Result.Username
 	return nil
 }
