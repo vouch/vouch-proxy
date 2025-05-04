3a96ee6770d0e7e47e3a6da04c6254356f2c7f54
Ahmed Aladeeb <ahmed.ayman.aladeeb@gmail.com>
2025-05-04 19:19:17 +0000

    switch to viper mapstructure because it's needed starting from viper v1.20, https://github.com/spf13/viper/blob/master/UPGRADE.md#v120x

--- pkg/cfg/cfg.go
+++ pkg/cfg/cfg.go
@@ -28,7 +28,7 @@ import (
 
 	"github.com/golang-jwt/jwt"
 	"github.com/kelseyhightower/envconfig"
-	"github.com/mitchellh/mapstructure"
+	"github.com/go-viper/mapstructure/v2"
 	"github.com/spf13/viper"
 	securerandom "github.com/theckman/go-securerandom"
 	"go.uber.org/zap"
@@ -375,7 +375,7 @@ func fixConfigOptions() {
 
 // use viper and mapstructure check to see if
 // https://pkg.go.dev/github.com/spf13/viper@v1.6.3?tab=doc#Unmarshal
-// https://pkg.go.dev/github.com/mitchellh/mapstructure?tab=doc#DecoderConfig
+// https://pkg.go.dev/github.com/mitchellh/mapstructure
 func checkConfigFileWellFormed() error {
 	opt := func(dc *mapstructure.DecoderConfig) {
 		dc.ErrorUnused = true
